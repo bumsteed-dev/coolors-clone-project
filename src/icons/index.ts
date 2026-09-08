0a2edf09f81e01ea998d0fbaf8a4c105f002cfc9
Jesús Cedeño Vélez <jesus.27velez@gmail.com>
2026-09-08 18:28:09 -0500

    feat: add GitHub repo link to header

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,3 +3,4 @@ export { default as IconSpaceBar } from './IconSpaceBar.astro';
 export { default as IconCopy } from './IconCopy.astro';
 export { default as IconCheckCircle } from './IconCheckCircle.astro';
 export { default as IconChevronDown } from './IconChevronDown.astro';
+export { default as IconGitHub } from './IconGitHub.astro';
