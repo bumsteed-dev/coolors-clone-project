645aea30334f094cf5d6c2fcf7c26e1e972656c5
Jesús Cedeño Vélez <jesus.27velez@gmail.com>
2026-09-07 22:12:32 -0500

    feat: split homepage into Header, Tools and Colors sections

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,3 +2,4 @@ export { default as IconRandom } from './IconRandom.astro';
 export { default as IconSpaceBar } from './IconSpaceBar.astro';
 export { default as IconCopy } from './IconCopy.astro';
 export { default as IconCheckCircle } from './IconCheckCircle.astro';
+export { default as IconChevronDown } from './IconChevronDown.astro';
