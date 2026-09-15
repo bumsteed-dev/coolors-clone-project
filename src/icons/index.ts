3d3de9f569c97b3542d08e71410b5f7c4d9b3fcd
Jesús Cedeño Vélez <jesus.27velez@gmail.com>
2026-09-14 22:20:42 -0500

    feat: export new shortcut icons and drop IconCopy re-export

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,6 +1,15 @@
 export { default as IconRandom } from './IconRandom.astro';
 export { default as IconSpaceBar } from './IconSpaceBar.astro';
-export { default as IconCopy } from './IconCopy.astro';
 export { default as IconCheckCircle } from './IconCheckCircle.astro';
 export { default as IconChevronDown } from './IconChevronDown.astro';
 export { default as IconGitHub } from './IconGitHub.astro';
+
+// Shorcuts icons
+export { default as IconRemove } from "./shortcuts/IconRemove.astro";
+export { default as IconCircleSplit } from "./shortcuts/IconCircleSplit.astro";
+export { default as IconSquareSplit } from "./shortcuts/IconSquareSplit.astro";
+export { default as IconHeart } from "./shortcuts/IconHeart.astro";
+export { default as IconArrows } from "./shortcuts/IconArrows.astro";
+export { default as IconCopy } from "./shortcuts/IconCopy.astro";
+export { default as IconInfo } from "./shortcuts/IconInfo.astro";
+export { default as IconUnlock } from "./shortcuts/IconUnlock.astro";
