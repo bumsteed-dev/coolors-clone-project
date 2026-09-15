8e43f3e9a3bc847f3e7c6a0baee90061cf09816c
Jesús Cedeño Vélez <jesus.27velez@gmail.com>
2026-09-14 22:20:42 -0500

    feat: add shortcuts data config for color card actions

---
+++ src/data/shortcuts.ts
@@ -0,0 +1,53 @@
+import {
+  IconRemove,
+  IconCircleSplit,
+  IconSquareSplit,
+  IconHeart,
+  IconArrows,
+  IconCopy,
+  IconInfo,
+  IconUnlock,
+} from "../icons";
+
+export const shortcuts = [
+  {
+    icon: IconRemove,
+    badge: "Remove color",
+    class: "remove-color"
+  },
+  {
+    icon: IconCircleSplit,
+    badge: "Check contrast",
+    class: "check-contrast"
+  },
+  {
+    icon: IconSquareSplit,
+    badge: "View shades",
+    class: "view-shades"
+  },
+  {
+    icon: IconHeart,
+    badge: "Save color",
+    class: "save-color"
+  },
+  {
+    icon: IconArrows,
+    badge: "Drag",
+    class: "drag"
+  },
+  {
+    icon: IconCopy,
+    badge: "Copy HEX",
+    class: "copy-hex"
+  },
+  {
+    icon: IconInfo,
+    badge: "View color info",
+    class: "color-info"
+  },
+  {
+    icon: IconUnlock,
+    badge: "Toggle lock",
+    class: "toggle-lock"
+  }
+];
